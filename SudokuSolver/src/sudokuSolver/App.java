dc039cc94d12f8f8ef25af908d663ba65ee75e12
StefanMemmel <98212548+StefanMemmel@users.noreply.github.com>
2022-01-22 17:31:12 +0100

    Initial Commit

---
+++ SudokuSolver/src/sudokuSolver/App.java
@@ -0,0 +1,69 @@
+package sudokuSolver;
+
+import java.util.Scanner;
+
+public class App {
+
+	private static Scanner scanner;
+
+	public static void main(String[] args) {
+// TODO Auto-generated method stub
+		System.out.println("This is commandline programm for solving sudokus ");
+		System.out.println(
+				"You have to enter your sudoku line by line. \n For empty cells please enter a zero (0). \n Don`t use blanks, just digits!");
+		System.out.println("---- Ready? Let`s start! ----");
+		commandlineApp();
+
+	}
+
+	private static void commandlineApp() {
+// TODO Auto-generated method stub
+		int[][] matrix = new int[9][9];
+
+		scanner = new Scanner(System.in);
+		String inputString = null;
+		int index = 0;
+		do {
+			System.out.println(
+					"Please enter the " + (index + 1) + ". line of your sudoku. For the unknown digit enter a 0");
+			inputString = scanner.nextLine();
+			if (isAccepted(inputString)) {
+				for (int c = 0; c < 9; c++) {
+					matrix[index][c] = Integer.parseInt(String.valueOf(inputString.charAt(c))); // (int)
+																								// inputString.charAt(index)-16;
+																								// //ASCII
+
+				}
+				index++;
+			}
+		} while (index < 9);
+		System.out.println("The Sudoku you entered is:");
+		Sudoku.printSolution(matrix);
+
+		System.out.println("The solution of your sudoku is:");
+		Sudoku.printSolution(Sudoku.solvePrepare(matrix));
+
+	}
+
+	private static boolean isAccepted(String inputString) {
+// TODO Auto-generated method stub
+		if (inputString.length() == 9) {
+
+			try {
+				int test = Integer.parseInt(inputString);
+
+			} catch (NumberFormatException e) {
+				System.out.println("You have entered an invalid character.");
+				return false;
+
+			}
+
+			return true;
+		} else {
+			System.out.println("Please enter exactly 9 digits!");
+		}
+
+		return false;
+	}
+
+}
---
+++ SudokuSolver/src/sudokuSolver/Sudoku.java
@@ -0,0 +1,194 @@
+package sudokuSolver;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+import javax.sql.rowset.RowSetWarning;
+
+public class Sudoku {
+private static ArrayList<int[]>  freeCells = new ArrayList<int[]>();
+ 
+public Sudoku(){
+	
+}
+
+	public static int [][] solvePrepare(int[][] matrix) {
+		// TODO Auto-generated method stub
+		/*add error cells to the list with the 0-cells and start to solve the sudoku*/
+		 int ErrorCell[]={-1,-1};
+		freeCells.add(ErrorCell);
+		return solve(matrix, 0);
+
+	}
+
+	static int getNumberFromBlock(int[][] matrix, int startDigit){
+		// Returns the lowest missing digit, that is greater then the startdigit
+		// there is no check whether the digit is a number <=9, that has to be done in the calling function 
+		int number =startDigit;
+		 
+		int row= matrix[0].length;
+		int column=matrix[1].length;
+		
+		boolean  FlagReturn=false;
+	 	do{
+			number++;
+			FlagReturn=false;
+			for(int r=0; r<row;r++){
+				for(int c=0; c<column;c++){
+					if(matrix[r][c]==number){
+						FlagReturn=true; // go on with the next Number
+						break;
+					}
+				}
+			}
+		  
+		}while(FlagReturn);  //while(FlagReturn && digit <10 ); False Intension 
+		
+		
+		return number;
+		
+	}
+	
+	static int[][] get3Block(int[][] matrix, int row, int column){
+ 
+		int [][]Block= new int [3][3];
+		
+		int rowStart= row - row%3; // dezimalstellen sollten automatisch wegfallen
+		int columnStart = column - column %3;
+		
+		for(int r=0; r<3; r++){
+			for(int c=0;c<3; c++ ){
+				Block[r][c]=matrix[rowStart+r][columnStart+c];
+			}
+		}
+		
+		
+		return Block;
+		
+	}
+	
+	
+
+//	static int[][]  solve(int[][] matrix, int startDigit){
+	private static int[][] solve(int[][] matrix, int startDigit){
+
+ 
+		int r_n, c_n, r_nminus1, c_nminus1; // if the 
+//		int startcell[]={r_nminus1, c_nminus1};
+//		freeCells.add(startcell); //  important !!!
+//		
+		
+		int	newDigit=startDigit;
+	 
+	  
+		while(findFirstZero(matrix)[0]!=-1){  // Abbruchkriterium keine 0 mehr in Matrix
+			
+			r_n=findFirstZero(matrix)[0];
+			c_n=findFirstZero(matrix)[1];
+ 
+			do{		
+				newDigit = getNumberFromBlock(get3Block(matrix, r_n, c_n),newDigit) ;  // hier ziehen aus Quadrat
+				if(newDigit<=9){
+				matrix[r_n][c_n]=newDigit;
+				}else{
+					r_nminus1= freeCells.get(freeCells.size()-1)[0];
+					c_nminus1= freeCells.get(freeCells.size()-1)[1];
+					freeCells.remove(freeCells.size()-1 ); //delete last element
+					//if(freeCells.size()>1){
+				//	 freeCells.remove(freeCells.size()-2); //delete element that was added before the current element
+					//}
+					 startDigit=matrix[r_nminus1][c_nminus1];
+					 matrix[r_nminus1][c_nminus1]=0;  // set last cell =0
+					matrix[r_n][c_n]=0;
+				 	solve(matrix,startDigit);
+				//	break;
+				}
+				
+			}while(rowAndColumnUnique(matrix, r_n, c_n)==false); 
+			int cell[]={r_n, c_n};  // !!!!
+			freeCells.add(cell); //add current Element
+			//printFreeCells(freeCells);
+			newDigit= 0; // start search for the digit in the next cell with newDigit=0
+			
+		}
+		return matrix;
+	}
+		
+	private static void printFreeCells(ArrayList<int[]> freeCells2) {
+	// TODO Auto-generated method stub
+		for(int i= 0; i<freeCells2.size(); i++){
+			System.out.print("("+freeCells2.get(i)[0]+"|"+freeCells2.get(i)[1]+") - " );
+		}
+		
+		
+	
+}
+
+	
+public static void printSolution(int[][] matrix) {
+		
+		
+		// TODO Auto-generated method stub
+
+		for(int r=0; r<matrix[0].length;r++){
+			for(int c=0; c<matrix[1].length;c++){ 
+			System.out.print(matrix[r][c] +" ");	
+				
+			}		
+			System.out.println();
+		}
+		
+	}
+	static int [] findFirstZero(int[][] matrix){
+		int result[]={0,0};
+		for(int r=0; r<9;r++){
+			boolean zero=false;
+			for(int c=0; c<9;c++){
+				if(matrix[r][c]==0){
+					//System.out.println(matrix[r][c] + " _");
+					result[0]=r;
+					result[1]=c;
+					zero=true;
+					break;
+				}
+			}
+			if(zero==true){
+				break;
+			}
+		if(matrix[8][8]!=0){
+		result[0]=-1; result[1]=-1; //no zeros in the array
+		}
+		}
+		
+		 
+		return result;
+	}
+	
+	/**
+	 *  checks if the value of the cell (r,c) is unique in column and row
+	 */
+	
+	private static boolean rowAndColumnUnique(int[][] matrix, int r, int c){
+		
+		for(int r_i=0; r_i < 9; r_i++){
+			if(r_i!=r && matrix[r_i][c]!=0){
+				if(matrix[r_i][c]==matrix[r][c]){
+					return false;
+				}
+			}			
+		}
+		
+		for(int c_i=0; c_i<9; c_i++){
+			if(c_i!=c && matrix[r][c_i]!=0 ){
+				if(matrix[r][c_i]==matrix[r][c]){
+					return false;
+				}
+			}
+		}	
+		return true;
+	}
+	
+	
+
+ 
+}
